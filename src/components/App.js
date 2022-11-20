2efe742274734453b36cf9344faefc1e5b42d283
sosumenia <sosumasita@gmail.com>
2022-11-20 16:41:39 +0900

    2. crud message

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,17 @@ import { authService } from "fbase";
 
 function App() {
    const [init, setInit] = useState(false);
-   const [isLoggedIn, setIsLoggedIn] = useState(false);
+   const [userObj, setUserObj] = useState(null);
+
    useEffect(() => {
       authService.onAuthStateChanged((user) => {
          if (user) {
-            setIsLoggedIn(true);
-         } else {
-            setIsLoggedIn(false);
+            setUserObj(user);
          }
          setInit(true);
       });
    }, []);
-   return <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}</>;
+   return <>{init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "initializing..."}</>;
 }
 
 export default App;
